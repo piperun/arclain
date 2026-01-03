0aed44d3caa12030952a8f7c4c3c77c8c2fb2eb4
0xdev <0xdev@noreply.codeberg.org>
2026-01-03 14:46:10 +0100

    Remove plugin build artifacts from repository

--- plugins/gstreamer-preview/release/build/serde-237bf408cfe7e0c1/build_script_build-237bf408cfe7e0c1.d
+++
@@ -1,5 +0,0 @@
-D:\Programming\rust\arclain\plugins\gstreamer-preview\.\release\build\serde-237bf408cfe7e0c1\build_script_build-237bf408cfe7e0c1.d: C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-D:\Programming\rust\arclain\plugins\gstreamer-preview\.\release\build\serde-237bf408cfe7e0c1\build_script_build-237bf408cfe7e0c1.exe: C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs
-
-C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.228\build.rs:
