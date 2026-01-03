77aec79f9c220a0826e5b122ddb8cac035da8e33
piperun <nevadadirren@gmail.com>
2026-01-03 14:46:10 +0100

    Remove plugin build artifacts from repository

--- plugins/dlsite-metadata/release/deps/unicode_ident-ddef65ba3cae94eb.d
+++
@@ -1,8 +0,0 @@
-D:\Programming\rust\arclain\plugins\dlsite-metadata\.\release\deps\unicode_ident-ddef65ba3cae94eb.d: C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\lib.rs C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\tables.rs
-
-D:\Programming\rust\arclain\plugins\dlsite-metadata\.\release\deps\libunicode_ident-ddef65ba3cae94eb.rlib: C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\lib.rs C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\tables.rs
-
-D:\Programming\rust\arclain\plugins\dlsite-metadata\.\release\deps\libunicode_ident-ddef65ba3cae94eb.rmeta: C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\lib.rs C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\tables.rs
-
-C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\lib.rs:
-C:\Users\Lechuck\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.22\src\tables.rs:
